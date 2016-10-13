5eddb8b55636f03f6e4f549f569984739336191a
John R. Forbes <jrforbes24@gmail.com>
2016-10-12 18:03:41 -0600

    intro to node and npm

--- Node/hey.js
+++ Node/hey.js
@@ -1,1 +1,11 @@
-console.log("hey node");
+var _ = require('underscore');
+var m2 = require('./module2');
+var http = require('http');
+
+var server = http.createServer(function(request, response){
+    console.log('got a request!');
+    response.write('hi');
+    response.end();
+});
+
+server.listen(3000);
---
+++ Node/module2.js
@@ -0,0 +1,8 @@
+var a = [500];
+var b = "Big bad John";
+
+
+module.exports = function() {
+    console.log(a);
+    console.log(b);
+};
