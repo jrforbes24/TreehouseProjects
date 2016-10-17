a117901b560eca217c6efcf6d39940586817e95b
John R. Forbes <jrforbes24@gmail.com>
2016-10-17 17:29:33 -0600

    set up node enviro

---
+++ Node/node_server/app.js
@@ -0,0 +1,12 @@
+// Problem: we need a simple way to look at a user's badge cound and JavaScript from a web browser.
+
+// Solution: Use nodejs to perform the profile look ups and server our templates via http
+
+// 1. Create a web server
+
+// 2. Handle the HTTP route  GET / and POST / i.e. Home
+    // 
+
+// 3. Handle the HTTP route /:username i.e. /chalkers
+
+// 4. Function that handles the reading of files and merge in values
