682e05aad3e678dc6cdefafe0a155104d4cb61a2
John R. Forbes <jrforbes24@gmail.com>
2016-10-17 18:44:35 -0600

    start node server code

--- Node/node_server/app.js
+++ Node/node_server/app.js
@@ -4,9 +4,36 @@
 
 // 1. Create a web server
 
-// 2. Handle the HTTP route  GET / and POST / i.e. Home
-    // 
+const http = require('http');
 
-// 3. Handle the HTTP route /:username i.e. /chalkers
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+// 2. Handle the HTTP route  GET / and POST / i.e. Home
+    // if url = "/" && GET
+        // show search
+    // if url = "/" && POST
+        // redirect to username
+
+// 3. Handle the HTTP route GET /:username i.e. /chalkers
+    // if url == "/...."
+        //get json from Treehouse
+            // on 'end'
+                // show profile
+            // on 'error'
+                // show error
 
 // 4. Function that handles the reading of files and merge in values
+    // read from file and get history
+        // merge values in to string
