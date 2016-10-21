c806f504071177bbb67e74c77ecdd41a1c756687
John R. Forbes <jrforbes24@gmail.com>
2016-10-20 18:32:30 -0600

    create a web server

--- Node/node_server/app.js
+++ Node/node_server/app.js
@@ -9,10 +9,13 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  setInterval(function(){
+      response.write(new Date() + "\n");
+ }, 1000);
+  //response.end('Hello World\n');
 });
 
 server.listen(port, hostname, () => {
