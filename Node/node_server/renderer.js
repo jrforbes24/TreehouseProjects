8e65c37f66566607a2a5b5ebd187ea61de8a92d3
John R. Forbes <jrforbes24@gmail.com>
2016-11-10 18:23:02 -0700

    add adtl info to node server files

--- Node/node_server/renderer.js
+++ Node/node_server/renderer.js
@@ -1,10 +1,20 @@
 var fs = require("fs");
 
+function mergeValues(values, content) {
+  // cycle over the keys
+  for(var key in vlaues){
+    // replace all key with the value from the values object
+    content = content.replace('{{" + key + "}}', values[key]);
+  }
+    // return merged content
+    return = content;
+}
+
 function view(templateName, values, response) {
   // read from the template files
-var fileContents =  fs.readFileSync('./views/' + templateName + '.html');
+var fileContents =  fs.readFileSync('./views/' + templateName + '.html',{encoding: "utf8"});
     // Insert vlaues in to the conent
-
+    fileContents = mergeValues(values, fileContents);
     // Write out to the response
     response.write(fileContents);
 }
